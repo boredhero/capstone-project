313e6c631b93eadff9b61c5e7c83964a310c2095
Noah Martino <noah@freedomgateway.llc>
2023-10-30 16:13:27 -0400

    add stuff

---
+++ ui.py
@@ -0,0 +1,11 @@
+import pygame
+import pygame.freetype
+from pygame.sprite import sprite
+from pygame.rect import Rect
+
+BLUE = (106, 159, 181)
+WHITE = (255, 255, 255)
+
+def create_surface_with_text(text: str, font_size: int, text_rgb: Tuple[r, g, b], ):
+    # https://programmingpixels.com/handling-a-title-screen-game-flow-and-buttons-in-pygame.html
+    pass
