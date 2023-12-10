286c5de3e8180ce89092b1761112654957f415f3
boredhero <personal.boredhero@gmail.com>
2023-12-10 02:42:18 -0500

    get collision for puzzle 1 working

--- puzzle_levels.py
+++ puzzle_levels.py
@@ -1,6 +1,8 @@
-import random
+from typing import Tuple
 
 import pygame
+
+from game_logger import GameLogger
 
 class GameMap:
 
@@ -70,14 +72,61 @@ class PuzzleHitbox:
         """
         self.visibility = True
         self.position = pos
+        self.color = (252, 0, 0)
+        self.original_color = (252, 0, 0)
+        self.collision_time = None
+        self.collision_duration = 8000 # milliseconds
+        self.is_currently_collided = False
+        self.__logger = GameLogger()
 
-    def draw(self, screen):
+    def update_color(self, screen, color: Tuple[int, int, int]):
+        """
+        Update the color of the hitbox
+        """
+        self.color = color
+        self.draw(screen, self.color)
+        pygame.display.flip()
+
+    def check_collision(self, screen, player: Player):
+        """
+        Check if player collides with hitbox
+        """
+        if self.visibility:
+            # Check for collision with the player
+            if (player.position[0] >= self.position[0] - 40 and
+                player.position[0] <= self.position[0] + 40 and
+                player.position[1] >= self.position[1] - 40 and
+                player.position[1] <= self.position[1] + 40):
+
+                # Collision detected, update color and record collision time
+                self.collision_time = pygame.time.get_ticks()
+                self.update_color(screen, (0, 252, 0))
+                self.is_currently_collided = True
+
+                # Log the collision
+                self.__logger.debug("Collision detected", f"PuzzleHitbox[(x: {self.position[0]}, y: {self.position[1]})]")
+                return True
+        return False
+
+    def draw(self, screen, color: Tuple[int, int, int] = None):
         """
         Draw the PuzzleHitBox
         """
         if self.visibility:
+            current_time = pygame.time.get_ticks()
+            # Check if the hitbox needs to revert back to its original color
+            if self.collision_time:
+                if current_time - self.collision_time > self.collision_duration:
+                    self.color = self.original_color  # Revert to original color
+                    self.collision_time = None
+                    self.is_currently_collided = False
+                else:
+                    color = (0, 252, 0)  # Keep the color green
+
+            # Use the current color if a specific color is not provided
+            final_color = self.color if color is None else color
             pygame.draw.circle(screen, (0, 0, 0), self.position, 40)
-            pygame.draw.circle(screen, (26, 255, 0), self.position, 30)
+            pygame.draw.circle(screen, final_color, self.position, 30)
 
     def set_visibility(self, visibility: bool):
         """
@@ -87,7 +136,7 @@ class PuzzleHitbox:
 
 class PuzzleHitboxGenerator:
 
-    def __init__(self, n_hitboxes: int, screen):
+    def __init__(self, hitbox_coords: list[Tuple], screen):
         """
         Puzzle Hitbox Generator
         """
@@ -95,14 +144,30 @@ class PuzzleHitboxGenerator:
         self.visibility = True
         self.hitboxes = []
         self.screen = screen
-        for _ in range(n_hitboxes):
-            x = random.randint(0, 1000)
-            y = random.randint(0, 1000)
+        for coords in hitbox_coords:
+            x = coords[0]
+            y = coords[1]
             self.hitboxes.append(PuzzleHitbox([x, y]))
         self.already_drawn = True
         if self.already_drawn:
             self.draw()
         self.draw()
+
+    def check_collision(self, player: Player):
+        """
+        Check if player collides with hitbox
+        """
+        for hitbox in self.hitboxes:
+            if hitbox.check_collision(self.screen, player):
+                hitbox.update_color(self.screen, (0, 252, 0))
+                return True
+        return False
+
+    def check_all_collided(self):
+        """
+        Check if all hitboxes are collided
+        """
+        return all(hitbox.is_currently_collided for hitbox in self.hitboxes)
 
     def draw(self):
         """
