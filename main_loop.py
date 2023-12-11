cfe71a63121b8ae1d0d827ffa8c389a5fabe10c3
boredhero <personal.boredhero@gmail.com>
2023-12-11 12:23:51 -0500

    refactor main_loop into a nice class

--- main_loop.py
+++ main_loop.py
@@ -31,21 +31,27 @@ class InstanceMainLoop:
             self.__main_loop()
         self.graceful_exit()
 
-    def __main_loop(self):
+    def __ml_handle_ginr(self):
         """
-        Meaty bit of the main loop
+        Handle reloading the game if we need to do that
         """
         if self.__ginr.needs_reload:
             self.__settings.refresh_from_disk()
             self.__ginr.set_needs_reload(False)
             self.__screen = pygame.display.set_mode((self.__settings.screen_width, self.__settings.screen_height))
             self.__init__() # pylint: disable=non-parent-init-called, unnecessary-dunder-call
-        mouse_up = False
+
+    def __ml_event_handler(self):
+        """
+        Event handler main loop
+        """
+        self.__mouse_up = False
         for event in pygame.event.get():
+            self.__current_event = event
             if event.type == pygame.QUIT:
                 self.__running = False
             if event.type == pygame.MOUSEBUTTONUP and event.button == 1:
-                mouse_up = True
+                self.__mouse_up = True
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_ESCAPE:
                     if self.__playing:
@@ -57,115 +63,153 @@ class InstanceMainLoop:
             if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Left mouse button
                 mouse_pos = pygame.mouse.get_pos()
                 self.__game_map_puzzle_2.hitbox_generator.check_click(mouse_pos)
-        if not self.check_playing_anything():
-            self.__screen.fill("black")
-            if self.__titlescreen_ui.visibility:
-                ui_action = self.__titlescreen_ui.update(pygame.mouse.get_pos(), mouse_up)
-                if ui_action is not None:
-                    match ui_action:
-                        case ui.GameState.EXIT:
-                            self.graceful_exit()
-                        case ui.GameState.SETTINGS:
-                            self.__gamesettings = SettingsMenu(self.__screen) # pylint: disable=unused-private-member
-                        case ui.GameState.PLAY:
-                            self.__titlescreen_ui.set_visibility(False)
-                            self.__playing = True
-                            self.__screen.fill((0, 0, 0))
-                        case ui.GameState.CREDITS:
-                            self.__titlescreen_ui.set_visibility(False)
-                            self.__show_credits = True
-                            self.__credits = text_screen.TextScreen(self.__screen, text_screen.get_credits_and_attributions_text(), "Back")
-                            self.__credits.draw()
-                        case ui.GameState.DEBUG_PLAY_PUZZLE:
-                            self.__titlescreen_ui.set_visibility(False)
-                            self.__debug_play_puzzles_ui.set_visibility(True)
-                        case _:
-                            pass
-            if self.__debug_play_puzzles_ui.visibility:
-                ui_action_levels = self.__debug_play_puzzles_ui.update(pygame.mouse.get_pos(), mouse_up)
-                if ui_action_levels is not None:
-                    match ui_action_levels:
-                        case ui.GameState.PLAY_PUZZLE_1:
-                            self.__show_text_screen_1 = True
-                            self.__debug_play_puzzles_ui.set_visibility(False)
-                            self.__text_screen_1 = text_screen.TextScreen(self.__screen, text_screen.get_puzzle_1_intro_text(), "Continue")
-                            self.__text_screen_1.draw()
-                        case ui.GameState.PLAY_PUZZLE_2:
-                            self.__show_text_screen_2 = True
-                            self.__debug_play_puzzles_ui.set_visibility(False)
-                            self.__text_screen_2 = text_screen.TextScreen(self.__screen, text_screen.get_puzzle_2_intro_text(), "Continue")
-                            self.__text_screen_2.draw()
-            if self.__show_text_screen_1:
-                self.__text_screen_1.draw()
-                if self.__text_screen_1.handle_event(event): # pylint: disable=undefined-loop-variable
-                    self.__show_text_screen_1 = False
-                    self.__playing_puzzle_1 = True
-            if self.__show_text_screen_2:
-                self.__text_screen_2.draw()
-                if self.__text_screen_2.handle_event(event): # pylint: disable=undefined-loop-variable
-                    self.__show_text_screen_2 = False
-                    self.__playing_puzzle_2 = True
-            if self.__show_credits:
+
+    def __handle_titlescreen_ui_action(self, ui_action):
+        """
+        Handle UI action events from the title screen
+        """
+        match ui_action:
+            case ui.GameState.EXIT:
+                self.graceful_exit()
+            case ui.GameState.SETTINGS:
+                self.__gamesettings = SettingsMenu(self.__screen) # pylint: disable=unused-private-member
+            case ui.GameState.PLAY:
+                self.__titlescreen_ui.set_visibility(False)
+                self.__playing = True
+                self.__screen.fill((0, 0, 0))
+            case ui.GameState.CREDITS:
+                self.__titlescreen_ui.set_visibility(False)
+                self.__show_credits = True
+                self.__credits = text_screen.TextScreen(self.__screen, text_screen.get_credits_and_attributions_text(), "Back")
                 self.__credits.draw()
-                if self.__credits.handle_event(event): # pylint: disable=undefined-loop-variable
-                    self.__titlescreen_ui.set_visibility(True)
-                    self.__show_credits = False
-            mouse_up = False
-        if self.__playing:
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_w] or keys[pygame.K_UP]:
-                self.__player_puzzle_1.move("up")
-            if keys[pygame.K_s] or keys[pygame.K_DOWN]:
-                self.__player_puzzle_1.move("down")
-            if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-                self.__player_puzzle_1.move("left")
-            if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-                self.__player_puzzle_1.move("right")
-            if keys[pygame.K_n]:
-                self.__game_map_puzzle_1.hitbox_generator.reset_hitboxes()
-            if self.__game_map_puzzle_1.all_hitboxes_collided():
-                self.return_to_main_menu()
-            self.__game_map_puzzle_1.draw_map()
-            self.__game_map_puzzle_1.hitbox_generator.set_collidability(True)
-            self.__game_map_puzzle_1.draw_hitboxes()
-            self.__player_puzzle_1.draw(self.__screen)
-            pygame.display.flip()
-        if self.__playing_puzzle_1:
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_w] or keys[pygame.K_UP]:
-                self.__player_puzzle_1.move("up")
-            if keys[pygame.K_s] or keys[pygame.K_DOWN]:
-                self.__player_puzzle_1.move("down")
-            if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-                self.__player_puzzle_1.move("left")
-            if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-                self.__player_puzzle_1.move("right")
-            if keys[pygame.K_n]:
-                self.__game_map_puzzle_1.hitbox_generator.reset_hitboxes()
-            if self.__game_map_puzzle_1.all_hitboxes_collided():
-                self.puzzle_1_return_to_main_menu()
-            self.__game_map_puzzle_1.draw_map()
-            self.__game_map_puzzle_1.hitbox_generator.set_collidability(True)
-            self.__game_map_puzzle_1.draw_hitboxes()
-            self.__player_puzzle_1.draw(self.__screen)
-            pygame.display.flip()
-        if self.__playing_puzzle_2:
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_n]:
-                self.__game_map_puzzle_2.hitbox_generator.reset_hitboxes()
-            self.__game_map_puzzle_2.hitbox_generator.update_hitbox_positions()
-            self.__game_map_puzzle_2.draw_map()
-            self.__game_map_puzzle_2.draw_hitboxes()
-            self.__game_map_puzzle_2.draw_message_box("What is your doctor's name so I can schedule an appointment?", self.__screen)
-            self.__game_map_puzzle_2.hitbox_generator.set_clickability(True)
-            if self.__game_map_puzzle_2.hitbox_generator.is_the_one_clicked():
-                self.puzzle_2_return_to_main_menu()
-            pygame.display.flip()
+            case ui.GameState.DEBUG_PLAY_PUZZLE:
+                self.__titlescreen_ui.set_visibility(False)
+                self.__debug_play_puzzles_ui.set_visibility(True)
+            case _:
+                pass
+
+    def __handle_level_selector_ui_action(self, ui_action):
+        """
+        Handle UI Action events from the level selector
+        """
+        match ui_action:
+            case ui.GameState.PLAY_PUZZLE_1:
+                self.__show_text_screen_1 = True
+                self.__debug_play_puzzles_ui.set_visibility(False)
+                self.__text_screen_1 = text_screen.TextScreen(self.__screen, text_screen.get_puzzle_1_intro_text(), "Continue")
+                self.__text_screen_1.draw()
+            case ui.GameState.PLAY_PUZZLE_2:
+                self.__show_text_screen_2 = True
+                self.__debug_play_puzzles_ui.set_visibility(False)
+                self.__text_screen_2 = text_screen.TextScreen(self.__screen, text_screen.get_puzzle_2_intro_text(), "Continue")
+                self.__text_screen_2.draw()
+
+    def __handle_show_text_screens(self):
+        """
+        Display any text screens, if conditions are correct for them to spawn
+        """
+        if self.__show_text_screen_1:
+            self.__text_screen_1.draw()
+            if self.__text_screen_1.handle_event(self.__current_event): # pylint: disable=undefined-loop-variable
+                self.__show_text_screen_1 = False
+                self.__playing_puzzle_1 = True
+        if self.__show_text_screen_2:
+            self.__text_screen_2.draw()
+            if self.__text_screen_2.handle_event(self.__current_event): # pylint: disable=undefined-loop-variable
+                self.__show_text_screen_2 = False
+                self.__playing_puzzle_2 = True
+        if self.__show_credits:
+            self.__credits.draw()
+            if self.__credits.handle_event(self.__current_event): # pylint: disable=undefined-loop-variable
+                self.__titlescreen_ui.set_visibility(True)
+                self.__show_credits = False
+        self.__mouse_up = False
+
+    def __blackfill_screen(self):
+        """
+        Hello darkness my old friend, I've come to talk with you again
+        """
+        self.__screen.fill("black")
+
+    def __handle_game_keyboard_input_puzzle_1(self):
+        """
+        Handle WASD, Arrow Keys, and N for resetting the hitboxes for Puzzle 1
+        """
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w] or keys[pygame.K_UP]:
+            self.__player_puzzle_1.move("up")
+        if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+            self.__player_puzzle_1.move("down")
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+            self.__player_puzzle_1.move("left")
+        if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
+            self.__player_puzzle_1.move("right")
+        if keys[pygame.K_n]:
+            self.__game_map_puzzle_1.hitbox_generator.reset_hitboxes()
+
+    def __puzzle_1_logic(self):
+        """
+        Logic for puzzle 1
+        """
+        self.__handle_game_keyboard_input_puzzle_1()
+        if self.__game_map_puzzle_1.all_hitboxes_collided():
+            self.puzzle_1_return_to_main_menu()
+        self.__game_map_puzzle_1.draw_map()
+        self.__game_map_puzzle_1.hitbox_generator.set_collidability(True)
+        self.__game_map_puzzle_1.draw_hitboxes()
+        self.__player_puzzle_1.draw(self.__screen)
+        pygame.display.flip()
+
+    def __puzzle_2_logic(self):
+        """
+        Logic for puzzle 2
+        """
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_n]:
+            self.__game_map_puzzle_2.hitbox_generator.reset_hitboxes()
+        self.__game_map_puzzle_2.hitbox_generator.update_hitbox_positions()
+        self.__game_map_puzzle_2.draw_map()
+        self.__game_map_puzzle_2.draw_hitboxes()
+        self.__game_map_puzzle_2.draw_message_box("What is your doctor's name so I can schedule an appointment?", self.__screen)
+        self.__game_map_puzzle_2.hitbox_generator.set_clickability(True)
+        if self.__game_map_puzzle_2.hitbox_generator.is_the_one_clicked():
+            self.puzzle_2_return_to_main_menu()
+        pygame.display.flip()
+
+    def __ml_handle_ui_if_not_playing(self):
+        """
+        If we aren't playing anything, we wanna do titlescreen stuff
+        """
+        self.__blackfill_screen()
+        if self.__titlescreen_ui.visibility:
+            titlescreen_ui_action = self.__titlescreen_ui.update(pygame.mouse.get_pos(), self.__mouse_up)
+            if titlescreen_ui_action is not None:
+                self.__handle_titlescreen_ui_action(titlescreen_ui_action)
+        if self.__debug_play_puzzles_ui.visibility:
+            level_selector_ui_action = self.__debug_play_puzzles_ui.update(pygame.mouse.get_pos(), self.__mouse_up)
+            if level_selector_ui_action is not None:
+                self.__handle_level_selector_ui_action(level_selector_ui_action)
         self.__titlescreen_ui.draw(self.__screen)
         if self.__debug_play_puzzles_ui.visibility:
             self.__debug_play_puzzles_ui.draw(self.__screen)
-        pygame.display.flip()
-        self.__clock.tick(self.__settings.max_fps) # Set the FPS
+
+    def __main_loop(self):
+        """
+        Meaty bit of the main loop
+        """
+        self.__ml_handle_ginr() # Handle the game needing a reload
+        self.__ml_event_handler() # Handle game events
+        if not self.check_playing_anything(): # If we're not playing anything, draw the titlescreen
+            self.__ml_handle_ui_if_not_playing() # Handle drawing various UI screens
+        self.__handle_show_text_screens() # If we need to display a text screen right now, display that
+        if self.__playing: # Play button
+            self.__puzzle_1_logic()
+        if self.__playing_puzzle_1: # Debug Puzzle Menu option 1
+            self.__puzzle_1_logic()
+        if self.__playing_puzzle_2: # Debug puzzle menu option 2
+            self.__puzzle_2_logic()
+        pygame.display.flip() # Necessary for UI to update
+        self.__clock.tick(self.__settings.max_fps) # Set the FPS and tick the clock at the end of each loop
 
     def pygame_init(self):
         """
@@ -195,6 +239,8 @@ class InstanceMainLoop:
         """
         Create private class variable defaults
         """
+        self.__mouse_up = False
+        self.__current_event = None
         self.__running = True
         self.__playing = False
         self.__playing_puzzle_1 = False
